b92af87aafa7594c1b2cb9d01e7c56ada7df0e21
David <paulussen86@gmail.com>
2024-01-25 12:59:34 +0100

    Gelöst ohne Klassendiagramm

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,38 @@ public class App {
     
     public static void main(String[] args) {
     
+        
+        Konto konto1 = new Konto();
+        konto1.kontostand = konto1.kontostand * 2;
+        output(String.valueOf(konto1.getKontostand()));
+        
+        
+        Konto konto2 = new Konto();
+        konto2.kontostand = konto2.kontostand * 3;
+        output(String.valueOf(konto2.getKontostand()));
+        
+        
+        Konto konto3 = new Konto();
+        konto3.kontostand = konto3.kontostand * 10;
+        output(String.valueOf(konto3.getKontostand()));
+        
+
+
     }
 
 
-    private static void output(String outputStr) {
+   
+    public static void output(String outputStr) {
         System.out.println(outputStr);
     } 
 
+
+    
 }
+
+
+
+
+
+
+
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,24 @@
+package com.cc.java;
+
+public class Konto {
+    
+    public int kontostand;
+
+    public Konto() {
+        this.kontostand = 10000;
+
+    }
+
+    public int getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand(int kontostand) {
+        this.kontostand = kontostand;
+    }
+
+    
+
+
+
+}

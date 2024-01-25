2d0d3ad194ca1499871d10ca08102ed6ecf35805
Geusen <155624924+Geusen@users.noreply.github.com>
2024-01-25 22:26:00 +0100

    Kontoinhaber  ++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,29 +5,40 @@ public class App {
     public static void main(String[] args) {
     
         
-        Konto konto1 = new Konto();
+
+
+        
+
+
+
+        Konto konto1 = new Konto("Geringverdiener", 10000);
         output("-------------------------------------------------------------");
-        output(String.valueOf("Konto Nr. 1 " + konto1.getKontostand()));
+        output(String.valueOf("Konto Nr. 1 " + konto1.getKontostand() + konto1.eu));
         konto1.kontostand = konto1.kontostand * 2;
         
 
 
-        Konto konto2 = new Konto();
-        
-        output(String.valueOf("Konto Nr. 2 " + konto2.getKontostand()));
+        Konto konto2 = new Konto("Kleinuntnernehmer", 10000);
+        output("-------------------------------------------------------------");
+        output(String.valueOf("Konto Nr. 2 " + konto2.getKontostand() + konto2.eu));
         konto2.kontostand = konto2.kontostand * 3;
         
-
-
-        Konto konto3 = new Konto();
         
-        output(String.valueOf("Konto Nr. 3 " + konto3.getKontostand()));
+
+        Konto konto3 = new Konto("Richkid", 10000);
+        output("-------------------------------------------------------------");
+        output(String.valueOf("Konto Nr. 3 " + konto3.getKontostand() + konto3.eu));
         konto3.kontostand = konto3.kontostand * 10;
         
         output("-------------------------------------------------------------");
-        output(String.valueOf("Konto Nr. 1 nach der Dividende " + konto1.getKontostand()));
-        output(String.valueOf("Konto Nr. 2 nach der Dividende " + konto2.getKontostand()));
-        output(String.valueOf("Konto Nr. 3 nach der Dividende " + konto3.getKontostand()));
+        output(String.valueOf(konto1.kontoInhaber));
+        output(String.valueOf("hat auf Konto Nr. 1 nach der Dividende " + konto1.getKontostand() + konto1.eu));
+        output("-------------------------------------------------------------");
+        output(String.valueOf(konto2.kontoInhaber));
+        output(String.valueOf("hat auf Konto Nr. 2 nach der Dividende " + konto2.getKontostand() + konto1.eu));
+        output("-------------------------------------------------------------");
+        output(String.valueOf(konto3.kontoInhaber));
+        output(String.valueOf("hat auf Konto Nr. 3 nach der Dividende " + konto3.getKontostand() + konto1.eu));
         output("-------------------------------------------------------------");
         
     }
--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -2,10 +2,15 @@ package com.cc.java;
 
 public class Konto {
     
+    public String kontoInhaber;
     public int kontostand;
+    public String eu;
 
-    public Konto() {
-        this.kontostand = 10000;
+    
+    public Konto(String kontoInhaber, int kontostand) {
+        this.kontoInhaber = kontoInhaber;
+        this.kontostand = kontostand;
+        eu = " Zaster";
 
     }
 
@@ -15,6 +20,14 @@ public class Konto {
 
     public void setKontostand(int kontostand) {
         this.kontostand = kontostand;
+    }
+
+    public String getKontoInhaber() {
+        return kontoInhaber;
+    }
+
+    public void setKontoInhaber(String kontoInhaber) {
+        this.kontoInhaber = kontoInhaber;
     }
 
     

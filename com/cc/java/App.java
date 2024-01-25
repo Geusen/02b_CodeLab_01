736080930d73c2550c8ab60797e68f38f8ca9d8b
David <paulussen86@gmail.com>
2024-01-25 15:43:15 +0100

    Vorher-Nachher Ausgabe

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,21 +6,28 @@ public class App {
     
         
         Konto konto1 = new Konto();
+        
+        output(String.valueOf("Konto Nr. 1 " + konto1.getKontostand()));
         konto1.kontostand = konto1.kontostand * 2;
-        output(String.valueOf(konto1.getKontostand()));
         
-        
+
+
         Konto konto2 = new Konto();
+        
+        output(String.valueOf("Konto Nr. 2 " + konto2.getKontostand()));
         konto2.kontostand = konto2.kontostand * 3;
-        output(String.valueOf(konto2.getKontostand()));
         
-        
+
+
         Konto konto3 = new Konto();
-        konto3.kontostand = konto3.kontostand * 10;
-        output(String.valueOf(konto3.getKontostand()));
         
+        output(String.valueOf("Konto Nr. 3 " + konto3.getKontostand()));
+        konto3.kontostand = konto3.kontostand * 10;
+        ;
 
-
+        output(String.valueOf("Konto Nr. 1 nach der Dividende " + konto1.getKontostand()));
+        output(String.valueOf("Konto Nr. 2 nach der Dividende " + konto2.getKontostand()));
+        output(String.valueOf("Konto Nr. 3 nach der Dividende " + konto3.getKontostand()));
     }
 
 
